e665bfe63598e4762049fa998562e72871c57ba9
Andrew Serra <acs8929@rit.edu>
2024-03-10 23:25:37 +0300

    successful db connection from go

--- backend/main.go
+++ backend/main.go
@@ -1,27 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// var db *sql.DB
+var db *sql.DB
 
 func main() {
 
-	// var err error
-	// db, err = sql.Open("mysql", "root:admin@tcp(127.0.0.1:3307)/books")
-	// defer db.Close()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	var err error
+	db, err = sql.Open("mysql", "root:admin@tcp(127.0.0.1:3307)/books")
 
-	// pingErr := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// if pingErr != nil {
-	// 	log.Fatal(pingErr)
-	// }
+	pingErr := db.Ping()
+
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
 
 	log.Print("Connected to the database.")
 }
