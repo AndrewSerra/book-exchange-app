5eb4596012e2124bd40960179cb8b4e12ddd4994
Andrew Serra <acs8929@rit.edu>
2024-04-17 00:02:13 +0300

    updated cron to run properly at every hour

--- scrapers/run.sh
+++ scrapers/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build -t book_scraper:1.0 
+docker build -t book_scraper:1.0 .
 
 docker run -id --mount type=bind,src=/Users/andrewserra/GithubProjects/book-exchange-app/scrapers/out,target=/usr/local/data/scraper --name book-scraper book_scraper:1.0
